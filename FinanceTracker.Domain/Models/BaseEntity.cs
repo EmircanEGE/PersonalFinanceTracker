bc9b1b273e0b3a0c23eca79c130390135da09e99
Emircan EGE <ege61emir@gmail.com>
2025-07-02 13:39:21 +0300

    feat(domain): Add UpdatedAt property to BaseEntity

--- FinanceTracker.Domain/Models/BaseEntity.cs
+++ FinanceTracker.Domain/Models/BaseEntity.cs
@@ -8,5 +8,7 @@ namespace FinanceTracker.Domain.Models
         public Guid Id { get; set; } = Guid.NewGuid();
         [Required]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
